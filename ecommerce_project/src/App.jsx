165c16c1937daf0b8b2cf9abfa8484934d07efed
Jan Francis Baldoza <you@example.com>
2023-02-10 01:20:53 +0800

    added: starter files

---
+++ ecommerce_project/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import { Navbar } from './components'
+
+function App() {
+
+
+  return (
+    <div className="app">
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
---
+++ ecommerce_project/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import './Navbar.scss'
+
+    const Navbar = () => {
+    return (
+        <nav className='app__navbar'>
+            <div className="app__navbar-container">
+                <div className="app__navbar-top">
+                    <div className="app__navbar-dropdown">
+                        <button>My Account<span class="material-symbols-outlined">expand_more</span></button>
+                        <div className="app__navbar-content">
+                            <p>Sign In</p>
+                            <p>Create an Account</p>
+                            <p>My Wishlist</p>
+                            <p>Compare List</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="app__navbar-bottom">
+                    <img src="/shirt.svg" alt="store logo" />
+                    <nav>
+                        <ul>
+                            <li>HOME</li>
+                            <li>ABOUT</li>
+                            <li>COLLECTIONS</li>
+                            <li>SHOP</li>
+                            <li>BESPOKE</li>
+                        </ul>
+                    </nav>
+                    <div className="app__navbar-icon">
+                        <span class="material-symbols-outlined">search</span> |
+                        <span class="material-symbols-outlined">shopping_cart</span>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
