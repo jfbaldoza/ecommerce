7df72af4fc988b861d3eb295baa71c28bf8ed81b
Jan Francis Baldoza <you@example.com>
2023-02-10 19:17:01 +0800

    added: main component

--- ecommerce_project/src/components/Main/Main.jsx
+++ ecommerce_project/src/components/Main/Main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './Main.scss'
 
 const Main = () => {
     return (
-        <div>
+        <div className='app__main'>
             <img src="https://cdn.shopify.com/s/files/1/0496/0544/3748/files/2.Banner_Frankie.jpg?v=1632929571" alt="" />
         </div>
     )
